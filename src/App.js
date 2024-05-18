31cab6d251796a1ec1332de8fffa828cb60fc890
Sharion2023 <Sharion@gmail.com>
2024-05-18 14:48:18 -0500

    I started and broke something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './App.css';
+import AddClass from './Components/AddClass';
+/*import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { all } from '@awesome.me/kit-KIT_CODE/icons'
+
+library.add(...all)*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Homeschool Hours Tracker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </h1>
+
+
+      <body>
+      <AddClass/>
+      </body>
+
+
     </div>
   );
 }
---
+++ src/Components/AddClass.js
@@ -0,0 +1,9 @@
+export default function AddClass () {
+    return (
+    <div>
+        <p>
+        Some drop downs
+        </p>
+    </div>
+    )
+}
