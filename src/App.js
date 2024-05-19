4f75ff0f2ff908327ae4b01d56c4ceefc96abc89
Sharion2023 <Sharion@gmail.com>
2024-05-19 13:57:38 -0500

    Buttons added. Need page setups

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 import './App.css';
-import AddClass from './Components/AddClass';
+import AddClass from './Components/AddHours';
 /*import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { all } from '@awesome.me/kit-KIT_CODE/icons'
@@ -18,10 +18,20 @@ function App() {
         </p>
       </h1>
 
+      <form class="form-inline" action="/action_page.php">
+        <div class="form-group">
+      
+    
+      <a href='/AddClass'>
+        <button type="button">Current Hours</button>
+        </a>
+      
+        <a href='/AddClass'>
+        <button type="button">Add Hours</button>
+        </a>
 
-      <body>
-      <AddClass/>
-      </body>
+      </div>
+      </form>
 
 
     </div>
