0ccaca319d7e8e509589510af82392aa46c79336
Sharion2023 <Sharion@gmail.com>
2024-05-21 12:26:33 -0500

    Dropdown added. Not functional.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 // components takes precedence over default styles.
 import './App.css';
 import AddClass from './Components/AddHours';
+import AddHours from './Components/AddHours';
 /*import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { all } from '@awesome.me/kit-KIT_CODE/icons'
@@ -11,30 +12,39 @@ library.add(...all)*/
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="App-header">
-        <p>
-          Welcome to Homeschool Hours Tracker
-        </p>
-      </h1>
+    
+    <div >
+      <main className='container'>
+        <div>
+          <h1 className="App-header">
+            <p>
+              Welcome to Homeschool Hours Tracker
+            </p>
+          </h1>
+        </div>
 
-      <form class="form-inline" action="/action_page.php">
-        <div class="form-group">
+        <div className='container'>
+
+          <form class="form-inline" action="/action_page.php">
+            <div class="form-group">
       
     
-      <a href='/AddClass'>
-        <button type="button">Current Hours</button>
-        </a>
+            <a className='btn btn-lg btn primary' href='/AddClass' role='button'>
+            Current Hours
+            </a>
       
-        <a href='/AddClass'>
-        <button type="button">Add Hours</button>
-        </a>
+           <a href='/AddClass'>
+            <button type="button">Add Hours</button>
+            </a>
 
-      </div>
-      </form>
+            </div>
+          </form>
+          <AddHours/>
+        </div>
 
-
+      </main>
     </div>
+  
   );
 }
 
