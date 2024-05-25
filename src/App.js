f23b3980b57e61d2a412aa0bc24888b06ce1dcdf
Sharion2023 <Sharion@gmail.com>
2024-05-24 20:05:47 -0500

    Added pages folder.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 import './App.css';
-import AddClass from './Components/AddHours';
-import AddHours from './Components/AddHours';
+import AddHours from './Pages/AddHours';
 /*import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { all } from '@awesome.me/kit-KIT_CODE/icons'
---
+++ src/Pages/AddHours.js
@@ -0,0 +1,17 @@
+export default function AddHours () {
+    return (
+        <form>
+            <div className="form-group">
+                <select class="custom-select custom-select-lg mb-3">
+                <option selected>Class Name</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+                </select>
+            </div>
+        </form>
+        
+        
+   
+    )
+}
