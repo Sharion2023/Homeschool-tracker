9ba1e0fe597b7b152f1d9183a9d54b3c9cddea49
Sharion2023 <Sharion@gmail.com>
2024-05-29 10:53:05 -0500

    Routes fixed and implemnted. Browser Router -->Router, Routes, Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
+import React from 'react';
+import {BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './App.css';
 import AddHours from './Pages/AddHours';
-import { Routes,Route, Router } from 'react-router-dom';
 import Home from './Pages/Home';
 import { Link } from 'react-router-dom';
 /*import ReactDOM from 'react-dom'
@@ -13,22 +14,18 @@ import { all } from '@awesome.me/kit-KIT_CODE/icons'
 library.add(...all)*/
 
 function App() {
-  return (
-    
-    
+  return (  
+      
+      
 
-    <div>
-          <AddHours/>
-
-        <Router>
+      <Router>       
           <Routes>
-            <Route path='/' element = {<Home/>}></Route>
-            <Route path= "/AddHours" element = {<AddHours/>}></Route>
+            <Route path='/' element = {<Home />} />
+            <Route path= '/AddHours' element = {<AddHours/>} />
           </Routes>
         </Router>
-
-     
-    </div>
+    
+        
   
   );
 }
