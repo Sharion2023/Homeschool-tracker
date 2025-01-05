6f2460e689cbd32a97e9d3d0d21ec6f354caf3dd
Sharion2023 <Sharion@gmail.com>
2025-01-05 13:50:55 -0600

    Updating add hours

--- src/Components/AddHours.js
+++ src/Components/AddHours.js
@@ -3,6 +3,9 @@
  export default function AddHours () {
      const [location, setLocation] = useState('');
      const [addHours, setAddHours] = useState(0);
+     const [current, setCurrentHours] = useState (0);
+     const [coreHours, setCoreHours] = useState (0);
+     const [regLocationHours, setRegLocationHours] = useState (0);
 
      const handleLocationChange = (event) => {
         setLocation(event.target.value);
@@ -19,23 +22,28 @@
          // Process the submitted text here
         console.log('location changed');
          console.log('Submitted location:', location);
+         console.log('hours changed');
+                  console.log('Submitted hours:', addHours);
          // Reset the text field after submission (optional)
-         setLocation('');
+         //setLocation('');
        };
 
        console.log(location)
         console.log('here');
-        console.log(addHours);
+        console.log(current);
 
 
     return (
         <form>
             <div className="form-group">
-                <select class="custom-select custom-select-lg mb-3">
+                <select class="custom-select custom-select-lg mb-3" value = {addHours} onChange= {handleHoursChange}>
                 <option selected>Hours to log</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option value="1">1</option>
+                <option value="1.5">1.5</option>
+                <option value="2">2</option>
+                <option value="2.5">2.5</option>
+                <option value="3">3</option>
+                <option value="3">3.5</option>
                 </select>
             </div>
 
--- src/Components/CurrentHours.js
+++ src/Components/CurrentHours.js
@@ -1,11 +1,17 @@
+import React, {useState} from 'react';
+import AddHours from './AddHours';
+
 export default function CurrentHours () {
+
+
+
 
     return(
     <>
         <p>Your current hours displayed</p>
-        <p>Cumulative Hours: </p>
-        <p>Core Hours: </p>
-        <p>Regular Location Hours: </p>
+        <p>Cumulative Hours: {AddHours.current} </p>
+        <p>Core Hours: {AddHours.coreHours} </p>
+        <p>Regular Location Hours: {AddHours.regLocationHours}</p>
     </>
     )
 }
